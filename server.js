d949b916e5eca5d938aa5a4d9822a07071c4d20a
Darren <hldaz77@gmail.com>
2023-07-27 14:01:50 +0100

    update

--- server.js
+++ server.js
@@ -1,28 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8080;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
 const app = express();
 
 app.use(cors());
-
-const data = require("./data/weather.json");
-
-class Forecast {
-  constructor(date, description) {
-    this.date = date;
-    this.description = description;
-  }
-}
 
 app.get("/", (req, res) => {
   res.send("I will be shown on the Browser");
   console.log("I will be shown on the Terminal");
 });
 
-app.get("/weather", (req, res) => {
+app.get("/weather", async (req, res) => {
   const { lat, lon, searchQuery } = req.query;
 
   console.log("Received query parameters:", lat, lon, searchQuery);
@@ -31,24 +25,78 @@ app.get("/weather", (req, res) => {
     return res.status(400).json({ error: "Missing query parameters" });
   }
 
-  const matchedCity = data.find(
-    (city) =>
-      city.city_name.toLowerCase() === searchQuery.toLowerCase() &&
-      parseFloat(city.lat) === parseFloat(lat) &&
-      parseFloat(city.lon) === parseFloat(lon)
-  );
+  try {
+    // Make an HTTPS request to Weatherbit API
+    const weatherResponse = await axios.get(
+      `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}`
+    );
 
-  console.log("Matched city:", matchedCity);
+    const { data } = weatherResponse.data;
 
-  if (!matchedCity) {
-    return res.status(404).json({ error: "City not found" });
+    if (!data || data.length === 0) {
+      return res.status(404).json({ error: "City not found" });
+    }
+
+    const forecasts = data.slice(0, 2).map((weatherData) => {
+      const forecast = new Forecast(
+        weatherData.valid_date,
+        weatherData.weather.description
+      );
+      return forecast;
+    });
+
+    res.json(forecasts);
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    return res.status(500).json({ error: "Error fetching weather data" });
+  }
+});
+
+// Route to fetch movies about each city
+app.get("/movies", async (req, res) => {
+  const { city } = req.query;
+
+  if (!city) {
+    return res.status(400).json({ error: "Missing city query parameter" });
   }
 
-  const forecasts = matchedCity.data.map(
-    (data) => new Forecast(data.valid_date, data.weather.description)
-  );
+  try {
+    // Fetch movies from the TMDB API using the city name
+    const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${encodeURIComponent(
+      city
+    )}`;
+    const movieResponse = await axios.get(movieUrl);
 
-  res.json(forecasts);
+    const movies = movieResponse.data.results.map((movie) => {
+      return new Movie(
+        movie.title,
+        movie.release_date,
+        movie.overview,
+        movie.poster_path
+      );
+    });
+
+    res.json(movies);
+  } catch (error) {
+    console.error("Error fetching movie data:", error);
+    return res.status(500).json({ error: "Error fetching movie data" });
+  }
 });
 
 app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
+
+class Forecast {
+  constructor(date, description) {
+    this.date = date;
+    this.description = description;
+  }
+}
+
+class Movie {
+  constructor(title, releaseDate, overview, posterPath) {
+    this.title = title;
+    this.releaseDate = releaseDate;
+    this.overview = overview;
+    this.posterPath = posterPath;
+  }
+}
